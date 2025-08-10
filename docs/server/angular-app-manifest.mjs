2f6f0287cc0ee4a998630ca76206946e26c8f020
dailoc77 <truongdailoc.dev@gmail.com>
2025-08-10 23:20:12 +0700

    new update

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,14 +7,34 @@ export default {
   routes: [
   {
     "renderMode": 2,
+    "redirectTo": "/web-security-scan-FE/auth",
     "route": "/web-security-scan-FE"
+  },
+  {
+    "renderMode": 2,
+    "route": "/web-security-scan-FE/auth"
+  },
+  {
+    "renderMode": 2,
+    "redirectTo": "/web-security-scan-FE/auth",
+    "route": "/web-security-scan-FE/login"
+  },
+  {
+    "renderMode": 2,
+    "redirectTo": "/web-security-scan-FE/auth",
+    "route": "/web-security-scan-FE/register"
+  },
+  {
+    "renderMode": 2,
+    "route": "/web-security-scan-FE/dashboard"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1078, hash: 'f3fe97ef038ad3ab156779fb1465a4c677a90d5aa37c416fa1369c0c8ff25b22', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1021, hash: '6cba8858fd25139e47a10d122e33c4d9a8f7af4edd811970088514e80921ceff', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 20175, hash: 'b7724e75d15de694a13a85ce244030aede83a3f95596c124caa32cf13da4919a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1078, hash: 'd950b915ce757fcac953b78f8bf6a14b276df3b7dcb25e6b196b72b87864c48e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1021, hash: 'f3e38d5775e44161426f6de70c22368da5f3a145c5c3362b933aae3a8fb26e76', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'auth/index.html': {size: 12569, hash: 'edbed247879204998cea9f0448817d0af38eb2488faf693c4fb1ddfcdb68ebec', text: () => import('./assets-chunks/auth_index_html.mjs').then(m => m.default)},
+    'dashboard/index.html': {size: 12569, hash: 'edbed247879204998cea9f0448817d0af38eb2488faf693c4fb1ddfcdb68ebec', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
     'styles-W4FRUZWP.css': {size: 445, hash: 'fDiF7V5zqW0', text: () => import('./assets-chunks/styles-W4FRUZWP_css.mjs').then(m => m.default)}
   },
 };
